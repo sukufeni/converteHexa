32f8d70b6c1f0728df69bfc964c60e6c2db0e9fa
bruno <bruno@NOTEBOOK-BRUNO>
2017-01-23 11:47:25 -0200

    a

--- TraducaoHexaText/Form1.cs
+++ TraducaoHexaText/Form1.cs
@@ -7,14 +7,36 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.IO;
 
 namespace TraducaoHexaText
 {
     public partial class Form1 : Form
     {
+        string[,] tabelaEntrada;
+        int linhaAtual;
+
         public Form1()
         {
             InitializeComponent();
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            StreamReader entrada = new StreamReader("C:/DDGIP.txt");
+            entrada.ReadLine();
+            int contador = 0;
+            tabelaEntrada = new string[71,17];
+            while (!entrada.EndOfStream)
+            {
+                string atual = entrada.ReadLine();
+                string[] verificacao = atual.Split('\t');
+                for(int x =0; x<verificacao.Length;x++)
+                {
+                    tabelaEntrada[contador, x] = verificacao[x];
+                }
+                contador++;
+            }
+            ManipulaArquivo arquivo = new ManipulaArquivo(this.tabelaEntrada);
         }
     }
 }
---
+++ TraducaoHexaText/ManipulaArquivo.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace TraducaoHexaText
+{
+    class ManipulaArquivo
+    {
+        private string entrada;
+        private string[,] saida, manipulado;
+        public ManipulaArquivo(string [,]entrada)
+        {
+            this.manipulado= entrada;
+            tabelaFinal();
+        }
+
+        private void tabelaFinal()
+        {
+            for(int x=0;x<manipulado.GetLength(0);x++)
+            {
+                entrada = manipulado[x, 2];
+                if(entrada!=null)
+                this.manipulado[x, 2] = converteHexa(entrada);
+            }
+            this.saida = manipulado;
+            salvaTabela(this.saida);
+        }
+        private string converteHexa(string entrada)
+        {
+            string resultado = "";
+            while (entrada.Length > 0)
+            {
+                resultado += System.Convert.ToChar(System.Convert.ToUInt32(entrada.Substring(0, 2), 16)).ToString();
+                entrada = entrada.Substring(2, entrada.Length - 2);
+            }
+            return resultado;
+        }
+        private void salvaTabela(string [,]saida)
+        {
+            StreamWriter caminhoSaida = new StreamWriter("//srvdc03/COMUM/CPD/DDGIP.txt", true);
+            string linhaAtual = "";
+            for(int x = 0; x < saida.GetLength(0); x++)
+            {
+                if (saida[x, 0] != null)
+                {
+                    for (int y = 0; y < saida.GetLength(1) - 1; y++)
+                    {
+                        linhaAtual += saida[x, y].ToString() + '\t';
+                    }
+                    caminhoSaida.WriteLine(linhaAtual);
+                    linhaAtual = "";
+                }
+                else
+                {
+                    caminhoSaida.Close();
+                }
+            }
+        }
+    }
+}
